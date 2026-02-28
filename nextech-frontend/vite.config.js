629c7a50e3f46852c17a1cb943f62324ada31b7f
Leonel Alcerro <alcerroleonel@gamil.com>
2026-02-27 21:14:46 -0600

    fix base url para github

--- nextech-frontend/vite.config.js
+++ nextech-frontend/vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Configuración de Vite para NexTech Honduras
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: true
-  }
+  base: '/NexTech/'
 })
